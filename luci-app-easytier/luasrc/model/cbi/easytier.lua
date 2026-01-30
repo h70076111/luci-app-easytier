acbafe561c4e55c89bba620aafac1c0d17e8a7f7
h70076111 <h70076222@2980.com>
2026-01-31 01:47:51 +0800

    Remove project description from easytier.lua
    Removed the description of the EasyTier project from the configuration file.

--- luci-app-easytier/luasrc/model/cbi/easytier.lua
+++ luci-app-easytier/luasrc/model/cbi/easytier.lua
@@ -1,12 +1,6 @@
 local http = luci.http
 local nixio = require "nixio"
-
-m = Map("easytier")
-m.description = translate("A simple, secure, decentralized VPN solution for intranet penetration, implemented in Rust using the Tokio framework. "
-        .. "Project URL: <a href=\"https://github.com/EasyTier/EasyTier\" target=\"_blank\">github.com/EasyTier/EasyTier</a>&nbsp;&nbsp;"
-        .. "<a href=\"http://easytier.cn\" target=\"_blank\">Official Documentation</a>&nbsp;&nbsp;"
-        .. "<a href=\"http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jhP2Z4UsEZ8wvfGPLrs0VwLKn_uz0Q_p&authKey=OGKSQLfg61YPCpVQuvx%2BxE7hUKBVBEVi9PljrDKbHlle6xqOXx8sOwPPTncMambK&noverify=0&group_code=949700262\" target=\"_blank\">QQ Group</a>&nbsp;&nbsp;")
-  
+ 
 m:section(SimpleSection).template  = "easytier/easytier_status"
 
 -- easytier-core
